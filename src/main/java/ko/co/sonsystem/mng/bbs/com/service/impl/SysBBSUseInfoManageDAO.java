082ecbcb1d2294b78132fa3923d084fc8aa8cde1
DESKTOP-5FTFAG9\hibiznet <ismartkorea@hotmail.com>
2023-11-23 15:36:07 +0900

    - 수정.

--- src/main/java/ko/co/sonsystem/mng/bbs/com/service/impl/SysBBSUseInfoManageDAO.java
+++ src/main/java/ko/co/sonsystem/mng/bbs/com/service/impl/SysBBSUseInfoManageDAO.java
@@ -10,20 +10,19 @@ import ko.co.sonsystem.mng.bbs.com.service.BoardUseInfVO;
 
 /**
  * 게시판 이용정보를 관리하기 위한 데이터 접근 클래스
- * @author 공통서비스개발팀 이삼섭
- * @since 2009.04.02
+ * @author s-onsystem
+ * @since 2023.11.16
  * @version 1.0
  * @see
- *
+ *  
  * <pre>
  * << 개정이력(Modification Information) >>
+ * 
+ *   수정일      수정자          수정내용
+ *  -------    --------    ---------------------------
+ *  2023.11.16  jeong          최초 생성
  *
- *   수정일       수정자           수정내용
- *  -------     --------    ---------------------------
- *   2009.04.02  이삼섭          최초 생성
- *   2011.05.31  JJY           경량환경 커스터마이징버전 생성
- *
- * </pre>
+ *  </pre>
  */
 @Repository("SysBBSUseInfoManageDAO")
 public class SysBBSUseInfoManageDAO extends EgovAbstractMapper {
--- src/main/java/ko/co/sonsystem/mng/bbs/com/service/impl/SysUserInfManageServiceImpl.java
+++ src/main/java/ko/co/sonsystem/mng/bbs/com/service/impl/SysUserInfManageServiceImpl.java
@@ -15,20 +15,19 @@ import ko.co.sonsystem.mng.bbs.com.service.UserInfVO;
 
 /**
  * 협업에서 사용할 사용자 조회 서비스 기능 구현 클래스
- * @author 공통서비스개발팀 이삼섭
- * @since 2009.04.06
+ * @author s-onsystem
+ * @since 2023.11.16
  * @version 1.0
  * @see
- *
+ *  
  * <pre>
  * << 개정이력(Modification Information) >>
- *   
- *   수정일      수정자           수정내용
+ * 
+ *   수정일      수정자          수정내용
  *  -------    --------    ---------------------------
- *   2009.04.06  이삼섭          최초 생성
- *   2011.08.31  JJY            경량환경 템플릿 커스터마이징버전 생성 
+ *  2023.11.16  jeong          최초 생성
  *
- * </pre>
+ *  </pre>
  */
 @Service("SysUserInfManageService")
 public class SysUserInfManageServiceImpl extends EgovAbstractServiceImpl implements SysUserInfManageService {
